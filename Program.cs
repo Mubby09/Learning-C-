643abdd30471cc9ecc8eb0b428b99b705b180bac
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-05-02 02:50:36 +0100

    learning

--- Program.cs
+++ Program.cs
@@ -6,14 +6,13 @@ namespace newToCsharp
     {
         static void Main(string[] args)
         {
-
-
             //Console.WriteLine("   /|");
             //Console.WriteLine("  / |");
             //Console.WriteLine(" /  |");
             //Console.WriteLine("/___|");
 
-            //Declaring variables
+            //*****Declaring variables*****
+
             //string characterName = "Mubarak";
             //int characterAge = 25;
             //char characterGrade = 'A';
@@ -35,7 +34,8 @@ namespace newToCsharp
 
 
 
-            //Returns a boolean value, True if the character is present in the string or false if the character is not present in the string
+            //*****Returns a boolean value, True if the character is present in the string or false if the character is not present in the string*****
+
             //Console.WriteLine(characterName.Contains("M"));
             //Console.WriteLine(characterName.Contains("G"));
 
@@ -53,7 +53,7 @@ namespace newToCsharp
 
 
 
-            //Getting input from User (.ReadLine() allows a user to input stuff into the terminal)
+            //*****Getting input from User (.ReadLine() allows a user to input stuff into the terminal)*****
 
             //Console.Write("Enter your name: ");
             //string name = Console.ReadLine();
@@ -66,7 +66,7 @@ namespace newToCsharp
 
 
 
-            //converting a string to an integer
+            //*****converting a string to an integer*****
 
             //int num = Convert.ToInt32("45");
             //Console.WriteLine(num + 6 );
@@ -90,8 +90,7 @@ namespace newToCsharp
 
 
 
-
-            //MAD LIBS
+            //*****MAD LIBS GAME*****
 
             //string color, pluralNoun, favoriteFootballer;
 
@@ -110,28 +109,174 @@ namespace newToCsharp
             //Console.WriteLine("I Love " + favoriteFootballer);
 
 
-            //ARRAYS
 
-            //creating an array of strings|| when we do not exactly what
+            //*****ARRAYS*****
+
+            //creating an array of strings. when we do not exactly what
             //elements that'll go into the array, we can simply instantiate it and give it a number,
             //...that tells C# how many elements we want the array to be abel to hold, Below we put 3,
-            //...that tells C# that we will be expecting 3 friends in the array
-            string[] myFriends = new string[3];
-            myFriends[0] = "Mariam";
-            myFriends[1] = "Zainab";
-            myFriends[2] = "Yinka";
-            
+            //...that tells C# that we will be expecting 3 friends (strings) in the array.
+
+            //string[] myFriends = new string[3];
+            //myFriends[0] = "Mariam";
+            //myFriends[1] = "Zainab";
+            //myFriends[2] = "Yinka";
+
 
             //If we attempt to put in more than the specified number of elements, C# throws an Out of range error
             //myFriends[3] = "Wale";
 
             //create an an of integers
-            int[] luckyNumbers = { 4, 7, 18, 90, 83, 2 };
+            //int[] luckyNumbers = { 4, 7, 18, 90, 83, 2 };
 
-            Console.WriteLine(luckyNumbers[4]);
+            //Console.WriteLine(luckyNumbers[4]);
 
+
+
+            //*****IF/ELSE STATEMENTS*****
+
+            //bool isMale = true;
+            //bool isTall = true;
+
+            //if (isMale && isTall)
+            //{
+            //    Console.WriteLine("Male child");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Female child or not tall or both");
+            //}
+
+
+            //*****A BETTER CALCULATOR*****
+            //Console.WriteLine("Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter operator: ");
+            //string op = Console.ReadLine();
+
+            //Console.WriteLine("Enter another number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //if (op == "+")
+            //{
+            //    Console.Write(num1 + num2);
+            //}
+            //else if (op == "-")
+            //{
+            //    Console.Write(num1 - num2);
+            //}
+            //else if(op == "/")
+            //{
+            //    Console.Write(num1 / num2);
+            //}
+            //else if (op == "*")
+            //{
+            //    Console.Write(num1 * num2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid operator");
+            //}
+
+
+            //*****WHILE LOOPS*****
+
+            int index = 1;
+
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+
+
+
+
+            //*****METHOD CALLS*****
+
+            //SayHi("Mubarak", 25);
+            //SayHi("Akinsola", 76);
+            //SayHi("Bolaji", 67);
+
+
+            //Console.WriteLine(Cube(3));
+
+            //Console.WriteLine(GetMax(7, 60));
+
+            //Console.WriteLine(GetDay(08));
 
             Console.ReadLine();
         }
+
+        //*****METHODS*****
+
+        //static void SayHi(string name,int age)
+        //{
+        //    Console.WriteLine("Hello " + name + " you are " + age );
+        //}
+
+
+        //*****YOU CAN ABSOLUTELY RETURN ANYTHING BASED ON THE DATA TYPE LIKE "STRING",
+        //..."DOUBLE", "FLOAT" LIKE "INT" BELOW*****
+
+        //static int Cube(int num)
+        //{
+        //    int result = num * num * num;
+        //    return result;
+        //}
+
+        //static int GetMax(int num1, int num2)
+        //{
+        //    int result;
+        //    if (num1 > num2)
+        //    {
+        //        result = num1;
+        //    }
+        //    else
+        //    {
+        //        result = num2;
+        //    }
+        //    return result;
+        //}
+
+          //*****SWITCH STATEMENTS*****
+
+        //static string GetDay(int dayNum)
+        //{
+        //    string dayName;
+
+        //    switch (dayNum)
+        //    {
+        //        case 0:
+        //            dayName = "Sunday";
+        //            break;
+        //        case 1:
+        //            dayName = "Monday";
+        //            break;
+        //        case 2:
+        //            dayName = "Tuesday";
+        //            break;
+        //        case 3:
+        //            dayName = "Wednesday";
+        //            break;
+        //        case 4:
+        //            dayName = "Thursday";
+        //            break;
+        //        case 5:
+        //            dayName = "Friday";
+                    //break;
+        //        case 6:
+        //            dayName = "Saturday";
+        //            break;
+        //        default:
+        //            dayName = "Invalid Day";
+        //            break;
+        //    }
+
+        //    return dayName;
+        //}
     }
+
 }
